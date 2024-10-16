027bbcfe93c528ecb214de0aa303fd781ab62471
rhtkdgus <rhtkdgus0103@gmail.com>
2024-10-16 14:07:58 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sanghyun Ko on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
